912948927e0d218f7fc54a63fb844c4f744495b2
Himanshu-sketch-design <himanshurana5800@gmail.com>
2025-03-22 15:58:49 +0530

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-MLOPS
+# MLOPS
+
+import dagshub
+dagshub.init(repo_owner='Himanshu-sketch-design', repo_name='MLOPS', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)  
--- app.py
+++ app.py
@@ -76,8 +76,10 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        remote_server_uri = ""
 
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+  
         # Model registry does not work with file store
         if tracking_url_type_store != "file":
             # Register the model
